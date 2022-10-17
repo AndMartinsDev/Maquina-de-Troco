65711be354371ba864ed48868aa96edc2f8afdab
André Luís Lopes Martins <107310422+AndMartinsDev@users.noreply.github.com>
2022-10-17 17:01:19 -0300

    Layout da Home Pronto

--- MqTroco/Data/SystemContext.cs
+++ MqTroco/Data/SystemContext.cs
@@ -5,18 +5,18 @@ namespace MqTroco.Data
 	public class SystemContext : DbContext
 	{
 
-		public SystemContext(DbContextOptions options):base(options)
+		public SystemContext(DbContextOptions options) : base(options)
 		{
 
 		}
 
-		public SystemContext():base()
+		public SystemContext() : base()
 		{
 
 		}
 
 		public DbSet<Caixa> Caixa { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			string conn = "Server=localhost\\SQLEXPRESS01;Database=DbMaquinaTroco;Trusted_Connection=True;";
 
@@ -27,5 +27,5 @@ namespace MqTroco.Data
 		}
 
 	}
-	
+
 }
