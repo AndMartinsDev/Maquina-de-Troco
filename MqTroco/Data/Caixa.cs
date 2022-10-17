06004d2b05e2ec22242b483fb577857338b8084f
André Luís Lopes Martins <107310422+AndMartinsDev@users.noreply.github.com>
2022-10-17 06:52:44 -0300

    Adicionar arquivos de projeto.

---
+++ MqTroco/Data/Caixa.cs
@@ -0,0 +1,15 @@
+﻿namespace MqTroco.Data
+{
+    public class Caixa
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public List<Moeda> Moedas { get; set; } = new List<Moeda>();
+
+
+        public Caixa(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ MqTroco/Data/Moeda.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+
+namespace MqTroco.Data
+{
+    public class Moeda
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public double Valor { get; set; }
+        public int Qtd { get; set; }
+
+    }
+}
