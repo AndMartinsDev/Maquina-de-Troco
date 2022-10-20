c5bd6fcb8d90f17e0e569c19970c81b14eb494f1
André Luís Lopes Martins <107310422+AndMartinsDev@users.noreply.github.com>
2022-10-20 14:33:24 -0300

    Criação do Db

--- MqTroco/Data/SystemContext.cs
+++ MqTroco/Data/SystemContext.cs
@@ -17,7 +17,7 @@ namespace MqTroco.Data
 		public DbSet<Moeda> Moeda { get; set; }
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			string conn = "Server=localhost\\SQLEXPRESS01;Database=DbMaquinaTroco;Trusted_Connection=True;";
+			string conn = "Server=localhost\\SQLEXPRESS01;Database=DbMaqTroco;Trusted_Connection=True;";
 
 			optionsBuilder.UseSqlServer(conn);
 
